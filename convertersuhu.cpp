788c24266c3997791ad8a10603260284fb99a345
naaa <89791645+renaramarosalinda@users.noreply.github.com>
2022-04-12 04:26:15 +0700

    Update convertersuhu.cpp

--- convertersuhu.cpp
+++ convertersuhu.cpp
@@ -22,8 +22,10 @@ int main()
     cout << "Masukkan derajat C : ";
     cin >> jawaban;
     total = (9/5 *jawaban) + 32;
-    cout<<"\n\n";
-    cout<<"Hasil konversi Celcius to Farenheit : "<<total<<"\n";
+    cout << "\n";
+    cout << "**************************************\n";
+    cout << "Hasil konversi Celcius to Farenheit : "<<total<<"\n";
+    cout << "**************************************\n\n";
     cout << "Apakah ingin menghitung ulang? \n";
     cout << "Tekan 1 jika ingin menghitung ulang \n";
     cout << "Tekan 0 jika ingin keluar \n";
@@ -38,8 +40,11 @@ int main()
 
     cout << "Masukkan derajat C : ";
     cin >> jawaban;
-    total = jawaban * 1.25;
-    cout << total << endl;
+    total = 4/5 * jawaban;
+    cout << "\n";
+    cout << "**************************************\n";
+    cout << "Hasil konversi Celcius to Reamur : "<<total<<"\n";
+    cout << "**************************************\n\n";
     cout << "Apakah ingin menghitung ulang? \n";
     cout << "Tekan 1 jika ingin menghitung ulang \n";
     cout << "Tekan 0 jika ingin keluar \n";
@@ -54,7 +59,10 @@ int main()
     cout << "Masukkan derajat F : ";
     cin >> jawaban;
     total = (jawaban - 32) * 5/9;
-    cout << total << endl;
+    cout << "\n";
+    cout << "**************************************\n";
+    cout << "Hasil konversi Farenheit to Celcius : "<<total<<"\n";
+    cout << "**************************************\n\n";
     cout << "Apakah ingin menghitung ulang? \n";
     cout << "Tekan 1 jika ingin menghitung ulang \n";
     cout << "Tekan 0 jika ingin keluar \n";
@@ -68,8 +76,11 @@ int main()
   else if (pilihan == 4){
     cout << "Masukkan derajat F : ";
     cin >> jawaban;
-    total = (jawaban - 32) * 5/9 + 273.15;
-    cout << total << endl;
+    total = (jawaban - 32) * 4/9;
+    cout << "\n";
+    cout << "**************************************\n";
+    cout<<"Hasil konversi Farenheit to Reamur : "<<total<<"\n";
+    cout << "**************************************\n\n";
     cout << "Apakah ingin menghitung ulang? \n";
     cout << "Tekan 1 jika ingin menghitung ulang \n";
     cout << "Tekan 0 jika ingin keluar \n";
@@ -83,8 +94,11 @@ int main()
   else if (pilihan == 5){
     cout << "Masukkan derajat R : ";
     cin >> jawaban;
-    total = jawaban - 273.15 ;
-    cout << total << endl;
+    total = 5/4 * jawaban;
+    cout << "\n";
+    cout << "**************************************\n";
+    cout << "Hasil konversi Reamur to Celcius : "<<total<<"\n";
+    cout << "**************************************\n\n";
     cout << "Apakah ingin menghitung ulang? \n";
     cout << "Tekan 1 jika ingin menghitung ulang \n";
     cout << "Tekan 0 jika ingin keluar \n";
@@ -98,8 +112,11 @@ int main()
   else if (pilihan == 6){
     cout << "Masukkan derajat R : ";
     cin >> jawaban;
-    total = (jawaban - 273.15) * 9/5 + 32 ;
-    cout << total << endl;
+    total = (9/4 * jawaban ) + 32;
+    cout << "\n";
+    cout << "**************************************\n";
+    cout << "Hasil konversi Reamur to Farenheit : "<<total<<"\n";
+    cout << "**************************************\n\n";
     cout << "Apakah ingin menghitung ulang? \n";
     cout << "Tekan 1 jika ingin menghitung ulang \n";
     cout << "Tekan 0 jika ingin keluar \n";
